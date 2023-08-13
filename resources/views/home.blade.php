3e6c0e06961f002f6e1a16edb1dc062ee1728e05
Mikhail Abramishvili <mg@abramishvili.net>
2023-08-13 18:50:19 +0400

    .

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -138,56 +138,7 @@
         </div>
     </div>
 
-    <div class="main-form p2">
-        <div class="container">
-            <div class="row align-items-center">
-                <div class="col-12 col-lg-4">
-                    <img src="/img/contact-circle.png" alt="">
-                </div>
-                <div class="col-12 col-lg-4">
-                    <div class="form-check">
-                        <input class="form-check-input" type="radio" name="transport" id="legkovoy">
-                        <label class="form-check-label" for="legkovoy">
-                            Для легкового авто
-                        </label>
-                    </div>
-
-                    <div class="form-check">
-                        <input class="form-check-input" type="radio" name="transport" id="krossover">
-                        <label class="form-check-label" for="krossover">
-                            Для кроссовера
-                        </label>
-                    </div>
-
-                    <div class="form-check">
-                        <input class="form-check-input" type="radio" name="transport" id="vnedorozhnik">
-                        <label class="form-check-label" for="vnedorozhnik">
-                            Для внедорожника
-                        </label>
-                    </div>
-
-                    <div class="form-check">
-                        <input class="form-check-input" type="radio" name="transport" id="spetstekhnika">
-                        <label class="form-check-label" for="spetstekhnika">
-                            Для спецтехники
-                        </label>
-                    </div>
-
-                    <div class="form-check">
-                        <input class="form-check-input" type="radio" name="transport" id="drugoi">
-                        <label class="form-check-label" for="drugoi">
-                            Для другого транспорта
-                        </label>
-                    </div>
-                </div>
-                <div class="col-12 col-lg-4">
-                    <input type="text" class="form-control" placeholder="Имя">
-                    <input type="text" class="form-control" placeholder="Телефон">
-                    <button class="btn btn-primary">Заказать со скидкой</button>
-                </div>
-            </div>
-        </div>
-    </div>
+    @include('mainform')
 
     <div class="bottom-text p1">
         <div class="container">
@@ -200,4 +151,6 @@
             <p>Вызвать эвакуатор круглосуточно в компании «Авто ангел» – правильный выбор, вы получите только лучшие услуги эвакуации и недорого! Если у вас возникнут вопросы, позвоните по телефону в любое время дня и ночи и проконсультируйтесь у наших специалистов. Они помогут вам определиться с выбором услуги. Обращайтесь, будем рады помочь!</p>
         </div>
     </div>
+
+    @include('mainform')
 @endsection
