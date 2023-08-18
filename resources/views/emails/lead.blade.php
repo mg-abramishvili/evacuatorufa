985f5593c43112b7c19a453f9a594cc56d582dd6
Mikhail Abramishvili <mg@abramishvili.net>
2023-08-18 12:35:51 +0400

    .

--- resources/views/emails/lead.blade.php
+++ resources/views/emails/lead.blade.php
@@ -1,14 +1,11 @@
 @component('mail::message')
 # Новая заявка с сайта evacuatorufa.ru
 
-{{ $text }}
-<br><br>
-
 Имя:<br>{{ $name }}
 <br><br>
 
 Телефон:<br>{{ $tel }}
 <br><br>
-----------
+{{ $text }}
 
 @endcomponent
