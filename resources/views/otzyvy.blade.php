5d669e81e251611a08df9851fd6b76760a8d1104
Mikhail Abramishvili <mg@abramishvili.net>
2023-11-03 14:46:25 +0400

    .

--- resources/views/otzyvy.blade.php
+++ resources/views/otzyvy.blade.php
@@ -7,7 +7,7 @@
         <div class="container">
             <h2 class="title-header mb-4">Отзывы</h1>
             
-            <div style="width:100%; height:1200px; overflow:hidden; position:relative;">
+            <div style="width: 760px; max-width:100%; height:1200px; overflow:hidden; position:relative;">
                 <iframe style="width:100%;height:100%;border:1px solid #e6e6e6;border-radius:8px;box-sizing:border-box" src="https://yandex.ru/maps-reviews-widget/177099743166?comments"></iframe>
             </div>
         </div>
