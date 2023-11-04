435e93b6f25c7d6ed89d6de95a311a035bcc3856
Mikhail Abramishvili <mg@abramishvili.net>
2023-11-04 13:15:01 +0400

    .

--- app/Http/Controllers/TelegramBotController.php
+++ app/Http/Controllers/TelegramBotController.php
@@ -12,11 +12,11 @@ class TelegramBotController extends Controller
     public function index(Request $request)
     {
         define('TOKEN', env('TELEGRAM_BOT_TOKEN'));
-
+        
         # Принимаем запрос
         $data = json_decode(file_get_contents('php://input'), TRUE);
-
-        file_put_contents(public_path('text.txt'), $request->all());
+        
+        file_put_contents(public_path('text.txt'), '$data: '.print_r($data, 1)."\n", FILE_APPEND);
 
         # Обрабатываем ручной ввод или нажатие на кнопку
         $data = $data['callback_query'] ? $data['callback_query'] : $data['message'];
