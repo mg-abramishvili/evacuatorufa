17c6bff21f8a856a1779a549ddb1b50c36bb9aa9
Mikhail Abramishvili <mg@abramishvili.net>
2023-11-04 15:26:41 +0400

    .

--- app/Http/Controllers/TelegramBotController.php
+++ app/Http/Controllers/TelegramBotController.php
@@ -80,8 +80,25 @@ class TelegramBotController extends Controller
 
     public function sendTelegram($method, $send_data)
     {
+        // file_put_contents(public_path('text.txt'), '$send_data: '.print_r($send_data, 1)."\n", FILE_APPEND);
+        
         define('TOKEN', env('TELEGRAM_BOT_TOKEN'));
 
-        file_put_contents(public_path('text.txt'), '$send_data: '.print_r($send_data, 1)."\n", FILE_APPEND);
+        $curl = curl_init();
+
+        curl_setopt_array($curl, [
+            CURLOPT_POST => 1,
+            CURLOPT_HEADER => 0,
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_URL => 'https://api.telegram.org/bot' . TOKEN . '/' . $method,
+            CURLOPT_POSTFIELDS => json_encode($send_data),
+            CURLOPT_HTTPHEADER => array_merge(array("Content-Type: application/json"), $headers)
+        ]);   
+        
+        $result = curl_exec($curl);
+
+        curl_close($curl);
+
+        return (json_decode($result, 1) ? json_decode($result, 1) : $result);
     }
 }
