a1c97f5467107fff2d430b8e18b1d6612de99845
Mikhail Abramishvili <mg@abramishvili.net>
2023-11-04 15:16:33 +0400

    .

--- app/Http/Controllers/TelegramBotController.php
+++ app/Http/Controllers/TelegramBotController.php
@@ -77,13 +77,13 @@ class TelegramBotController extends Controller
 
         $send_data['chat_id'] = $data['chat']['id'];
 
-        file_put_contents('text.txt', '$send_data: '.print_r($send_data, 1)."\n", FILE_APPEND);
-
-        // $this->sendTelegram($method, $send_data);
+        $this->sendTelegram($method, $send_data);
     }
 
     public function sendTelegram($method, $send_data)
     {
         define('TOKEN', env('TELEGRAM_BOT_TOKEN'));
+
+        file_put_contents(public_path('text.txt'), '$send_data: '.print_r($send_data, 1)."\n", FILE_APPEND);
     }
 }
