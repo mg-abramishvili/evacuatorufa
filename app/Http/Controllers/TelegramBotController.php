aa73f746594d46082fd3edea63ae9ee9b52b292c
Mikhail Abramishvili <mg@abramishvili.net>
2023-11-05 13:59:55 +0400

    .

--- app/Http/Controllers/TelegramBotController.php
+++ app/Http/Controllers/TelegramBotController.php
@@ -15,7 +15,7 @@ class TelegramBotController extends Controller
     {
         $data = $request->callback_query ? $request->callback_query : $request->message;
 
-        // $message = mb_strtolower(($data['text'] ? $data['text'] : $data['data']), 'utf-8');
+        $message = mb_strtolower(($data['text'] ? $data['text'] : $data['data']), 'utf-8');
 
         return file_put_contents(public_path('text.txt'), print_r($data, 1)."\n", FILE_APPEND);
 
