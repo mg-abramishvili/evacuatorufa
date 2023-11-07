f9cdde8ce5e9b46166010bfc53c5dacbd18a385c
Mikhail Abramishvili <mg@abramishvili.net>
2023-11-07 13:03:59 +0400

    .

--- app/Http/Controllers/TelegramBotController.php
+++ app/Http/Controllers/TelegramBotController.php
@@ -128,6 +128,15 @@ class TelegramBotController extends Controller
             $method = 'sendPhoto';
             $sendData = [
                 'photo' => 'https://evacuatorufa.ru/img/evc01.jpg',
+                'photo' => 'https://evacuatorufa.ru/img/evc02.jpg',
+                'photo' => 'https://evacuatorufa.ru/img/evc03.jpg',
+                'photo' => 'https://evacuatorufa.ru/img/evc04.jpg',
+                'photo' => 'https://evacuatorufa.ru/img/evc05.jpg',
+                'photo' => 'https://evacuatorufa.ru/img/evc06.jpg',
+                'photo' => 'https://evacuatorufa.ru/img/evc07.jpg',
+                'photo' => 'https://evacuatorufa.ru/img/evc08.jpg',
+                'photo' => 'https://evacuatorufa.ru/img/evc09.jpg',
+                'photo' => 'https://evacuatorufa.ru/img/evc10.jpg',
             ];
         }
 
