9505a0c7c8928d48a29ef2cd9eefdde2734e5202
Mikhail Abramishvili <mg@abramishvili.net>
2023-11-07 21:03:40 +0400

    .

--- app/Http/Controllers/TelegramBotController.php
+++ app/Http/Controllers/TelegramBotController.php
@@ -204,6 +204,18 @@ class TelegramBotController extends Controller
             $method = 'sendMessage';
             $sendData = [
                 'text'   => "Вы вызываете " . $telegramBotLog->transport . " по адресу " . $telegramBotLog->address . "\n\n Ваш номер телефона: " . $message . "\n\n Всё верно? 🤔",
+                'reply_markup' => [
+                    'inline_keyboard' => [
+                        [
+                            'text' => 'Да, подтверждаю!',
+                            'callback_data' => 'your_order_yes',
+                        ],
+                        [
+                            'text' => 'Отменить заявку',
+                            'callback_data' => 'your_order_no',
+                        ]
+                    ],
+                ]
             ];
 
             $telegramBotLog->tel = $message;
